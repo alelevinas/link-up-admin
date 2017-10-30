3bb20d4f125c989c37b793897924cf5cf92ea33d
Nico Moccagatta <nicomoccagatta@gmail.com>
2017-10-30 03:24:25 -0300

    Fix for getting reports

--- src/users/LinkToRelatedReports.js
+++ src/users/LinkToRelatedReports.js
@@ -14,7 +14,7 @@ const LinkToRelatedReports = ({ record, translate }) => (
         containerElement={<Link
             to={{
                 pathname: '/reports',
-                search: stringify({ page: 1, perPage: 25, filter: JSON.stringify({ userIdReporter: record.id }) }),
+                search: stringify({ page: 1, perPage: 25, filter: JSON.stringify({ userId: record.id }) }),
             }}
         />}
     />
